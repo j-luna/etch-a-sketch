7a50467c2064419255a89d6b19862cd819c10656
Joe Lunaburg <38874911+j-luna@users.noreply.github.com>
2025-08-28 18:17:55 -0500

    Adjust grid sizes and add grid size button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,25 +1,45 @@
-const SIDELEN = 16;
-const container = document.querySelector("#container");
+function drawGrid (sideLen) {
+    const container = document.querySelector("#container");
 
-for (let i = 0; i < SIDELEN; i++) {
-    const gridRow = document.createElement("div");
-    gridRow.classList.add("row")
-    container.appendChild(gridRow);
-    for (let j = 0; j < SIDELEN; j++) {
-        const gridDiv = document.createElement("div");
-        // gridDiv.textContent = "Test";
-        gridDiv.classList.add("grid-div");
-        gridRow.appendChild(gridDiv);
+    for (let i = 0; i < sideLen; i++) {
+        const gridRow = document.createElement("div");
+        gridRow.classList.add("row")
+        container.appendChild(gridRow);
+        for (let j = 0; j < sideLen; j++) {
+            const gridDiv = document.createElement("div");
+            // gridDiv.textContent = "Test";
+            gridDiv.classList.add("grid-div");
+            gridRow.appendChild(gridDiv);
+        }
+    }
+
+    const gridDivList = document.querySelectorAll(".grid-div");
+    for (let gridDiv of gridDivList) {
+        gridDiv.addEventListener("mouseover", function (e) {
+            e.target.classList.add("grid-div-colored");
+        })
+
+        // gridDiv.addEventListener("mouseout", function (e) {
+        //    e.target.classList.toggle("grid-div-colored");
+        //})
     }
 }
 
-const gridDivList = document.querySelectorAll(".grid-div");
-for (let gridDiv of gridDivList) {
-    gridDiv.addEventListener("mouseover", function (e) {
-        e.target.classList.add("grid-div-colored");
-    })
+let gridSize = 16;
+drawGrid(gridSize);
 
-    // gridDiv.addEventListener("mouseout", function (e) {
-    //    e.target.classList.toggle("grid-div-colored");
-    //})
-}
+const gridSizeButton = document.querySelector("button");
+gridSizeButton.addEventListener('click', function (e) {
+    let invalidGridSize = true;
+    while (invalidGridSize) {
+        gridSize = prompt("Please enter a new grid size");
+        if ( (gridSize > 0) && (gridSize <= 100) ) {
+            invalidGridSize = false;
+        }
+        else {
+            alert("Invalid grid size selected!");
+        }
+    }
+    container.replaceChildren();
+    drawGrid(gridSize);
+})
