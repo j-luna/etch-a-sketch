53966f0153c55b6ba584a01f855a160c5e0f8b56
Joe Lunaburg <38874911+j-luna@users.noreply.github.com>
2025-08-28 17:58:18 -0500

    Add grid flexbox and hover functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,8 +1,25 @@
-const GRIDSIZE = 16;
+const SIDELEN = 16;
 const container = document.querySelector("#container");
 
-for (let i = 0; i < GRIDSIZE; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.textContent = "Test";
-    container.appendChild(gridDiv);
+for (let i = 0; i < SIDELEN; i++) {
+    const gridRow = document.createElement("div");
+    gridRow.classList.add("row")
+    container.appendChild(gridRow);
+    for (let j = 0; j < SIDELEN; j++) {
+        const gridDiv = document.createElement("div");
+        // gridDiv.textContent = "Test";
+        gridDiv.classList.add("grid-div");
+        gridRow.appendChild(gridDiv);
+    }
+}
+
+const gridDivList = document.querySelectorAll(".grid-div");
+for (let gridDiv of gridDivList) {
+    gridDiv.addEventListener("mouseover", function (e) {
+        e.target.classList.add("grid-div-colored");
+    })
+
+    // gridDiv.addEventListener("mouseout", function (e) {
+    //    e.target.classList.toggle("grid-div-colored");
+    //})
 }
